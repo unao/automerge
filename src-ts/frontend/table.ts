db8c54d642ca4875f5df9315c8c1de4777f18913
unao <artur@unao.pl>
2018-12-14 21:18:10 +0100

    frontend/table ~> .ts

--- src-ts/frontend/table.ts
+++ src-ts/frontend/table.ts
@@ -1,21 +1,24 @@
-const { OBJECT_ID, CONFLICTS } = require('./constants')
-const { isObject } = require('../common')
+import { ObjectId } from '../types'
 
-function compareRows(properties, row1, row2) {
+import { OBJECT_ID, CONFLICTS } from './constants'
+import { isObject } from '../common'
+import { Context } from './context'
+
+function compareRows (properties: any, row1: any, row2: any) {
   for (let prop of properties) {
     if (row1[prop] === row2[prop]) continue
 
     if (typeof row1[prop] === 'number' && typeof row2[prop] === 'number') {
       return row1[prop] - row2[prop]
     } else {
-      const prop1 = '' + row1[prop], prop2 = '' + row2[prop]
+      const prop1 = '' + row1[prop]
+      const prop2 = '' + row2[prop]
       if (prop1 === prop2) continue
       if (prop1 < prop2) return -1; else return +1
     }
   }
   return 0
 }
-
 
 /**
  * A relational-style collection of records. A table has an ordered list of
@@ -24,11 +27,12 @@ function compareRows(properties, row1, row2) {
  * object ID to row object.
  */
 export class Table {
+  entries: any
   /**
    * This constructor is used by application code when creating a new Table
    * object within a change callback.
    */
-  constructor(columns) {
+  constructor (public columns: string[]) {
     if (!Array.isArray(columns)) {
       throw new TypeError('When creating a table you must supply a list of columns')
     }
@@ -40,7 +44,7 @@ export class Table {
   /**
    * Looks up a row in the table by its unique ID.
    */
-  byId(id) {
+  byId (id: ObjectId) {
     return this.entries[id]
   }
 
@@ -48,7 +52,7 @@ export class Table {
    * Returns an array containing the unique IDs of all rows in the table, in no
    * particular order.
    */
-  get ids() {
+  get ids () {
     return Object.keys(this.entries).filter(key => {
       const entry = this.entries[key]
       return isObject(entry) && entry[OBJECT_ID] === key
@@ -58,7 +62,7 @@ export class Table {
   /**
    * Returns the number of rows in the table.
    */
-  get count() {
+  get count () {
     return this.ids.length
   }
 
@@ -66,7 +70,7 @@ export class Table {
    * Returns an array containing all of the rows in the table, in no particular
    * order.
    */
-  get rows() {
+  get rows () {
     return this.ids.map(id => this.byId(id))
   }
 
@@ -74,7 +78,7 @@ export class Table {
    * The standard JavaScript `filter()` method, which passes each row to the
    * callback function and returns all rows for which the it returns true.
    */
-  filter(callback, thisArg) {
+  filter (callback: (value: any, index: number, array: any[]) => any, thisArg: any) {
     return this.rows.filter(callback, thisArg)
   }
 
@@ -82,7 +86,7 @@ export class Table {
    * The standard JavaScript `find()` method, which passes each row to the
    * callback function and returns the first row for which it returns true.
    */
-  find(callback, thisArg) {
+  find (callback: (value: any, index: number, array: any[]) => any, thisArg: any) {
     return this.rows.find(callback, thisArg)
   }
 
@@ -90,21 +94,21 @@ export class Table {
    * The standard JavaScript `map()` method, which passes each row to the
    * callback function and returns a list of its return values.
    */
-  map(callback, thisArg) {
+  map (callback: (value: any, index: number, array: any[]) => any, thisArg: any) {
     return this.rows.map(callback, thisArg)
   }
 
   /**
-  * Returns the list of rows, sorted by one of the following:
-  * - If a function argument is given, it compares rows as per
-  *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Description
-  * - If a string argument is given, it is interpreted as a column name and
-  *   rows are sorted according to that column.
-  * - If an array of strings is given, it is interpreted as a list of column
-  *   names, and rows are sorted lexicographically by those columns.
-  * - If no argument is given, it sorts by row ID by default.
-  */
-  sort(arg) {
+   * Returns the list of rows, sorted by one of the following:
+   * - If a function argument is given, it compares rows as per
+   *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Description
+   * - If a string argument is given, it is interpreted as a column name and
+   *   rows are sorted according to that column.
+   * - If an array of strings is given, it is interpreted as a list of column
+   *   names, and rows are sorted lexicographically by those columns.
+   * - If no argument is given, it sorts by row ID by default.
+   */
+  sort (arg: ((a: any, b: any) => number) | string | Array<string> | undefined) {
     if (typeof arg === 'function') {
       return this.rows.sort(arg)
     } else if (typeof arg === 'string') {
@@ -123,14 +127,15 @@ export class Table {
    * particular order.
    */
   [Symbol.iterator] () {
-    let rows = this.rows, index = -1
+    let rows = this.rows
+    let index = -1
     return {
       next () {
         index += 1
         if (index < rows.length) {
-          return {done: false, value: rows[index]}
+          return { done: false, value: rows[index] }
         } else {
-          return {done: true}
+          return { done: true }
         }
       }
     }
@@ -141,17 +146,17 @@ export class Table {
    * a patch to the table, and `freeze()` is called on it when we have finished
    * applying the patch.
    */
-  _clone() {
-    if (!this[OBJECT_ID]) {
+  _clone () {
+    if (!(this as any)[OBJECT_ID]) {
       throw new RangeError('clone() requires the objectId to be set')
     }
-    return instantiateTable(this[OBJECT_ID], Object.assign({}, this.entries))
+    return instantiateTable((this as any)[OBJECT_ID], Object.assign({}, this.entries))
   }
 
   /**
    * Sets the entry with key `id` to `value`.
    */
-  set(id, value) {
+  set (id: ObjectId, value: any) {
     if (Object.isFrozen(this.entries)) {
       throw new Error('A table can only be modified in a change function')
     }
@@ -162,7 +167,7 @@ export class Table {
   /**
    * Removes the row with unique ID `id` from the table.
    */
-  remove(id) {
+  remove (id: ObjectId) {
     if (Object.isFrozen(this.entries)) {
       throw new Error('A table can only be modified in a change function')
     }
@@ -172,7 +177,7 @@ export class Table {
   /**
    * Makes this object immutable. This is called after a change has been made.
    */
-  _freeze() {
+  _freeze () {
     Object.freeze(this.entries)
     Object.freeze(this)
   }
@@ -182,13 +187,13 @@ export class Table {
    * the table is accessed within a change callback. `context` is the proxy
    * context that keeps track of the mutations.
    */
-  getWriteable(context) {
-    if (!this[OBJECT_ID]) {
+  getWriteable (context: Context) {
+    if (!(this as any)[OBJECT_ID]) {
       throw new RangeError('getWriteable() requires the objectId to be set')
     }
 
     const instance = Object.create(WriteableTable.prototype)
-    instance[OBJECT_ID] = this[OBJECT_ID]
+    instance[OBJECT_ID] = (this as any)[OBJECT_ID]
     instance.context = context
     instance.entries = this.entries
     return instance
@@ -200,11 +205,12 @@ export class Table {
  * callback.
  */
 class WriteableTable extends Table {
+  context!: Context
   /**
    * Returns a proxied version of the columns list. This list can be modified
    * within a change callback.
    */
-  get columns() {
+  get columns () {
     const columnsId = this.entries.columns[OBJECT_ID]
     return this.context.instantiateObject(columnsId)
   }
@@ -213,7 +219,7 @@ class WriteableTable extends Table {
    * Returns a proxied version of the row with ID `id`. This row object can be
    * modified within a change callback.
    */
-  byId(id) {
+  byId (id: ObjectId) {
     if (isObject(this.entries[id]) && this.entries[id][OBJECT_ID] === id) {
       return this.context.instantiateObject(id)
     }
@@ -225,24 +231,25 @@ class WriteableTable extends Table {
    * values, it is translated into a map using the table's column list.
    * Returns the objectId of the new row.
    */
-  add(row) {
+  add (row: any) {
     if (Array.isArray(row)) {
-      const columns = this.columns, rowObj = {}
+      const columns = this.columns
+      const rowObj = {} as any
       for (let i = 0; i < columns.length; i++) {
         rowObj[columns[i]] = row[i]
       }
       row = rowObj
     }
-    return this.context.addTableRow(this[OBJECT_ID], row)
+    return this.context.addTableRow((this as any)[OBJECT_ID], row)
   }
 
   /**
    * Removes the row with ID `id` from the table. Throws an exception if the row
    * does not exist in the table.
    */
-  remove(id) {
+  remove (id: ObjectId) {
     if (isObject(this.entries[id]) && this.entries[id][OBJECT_ID] === id) {
-      this.context.deleteTableRow(this[OBJECT_ID], id)
+      this.context.deleteTableRow((this as any)[OBJECT_ID], id)
     } else {
       throw new RangeError(`There is no row with ID ${id} in this table`)
     }
@@ -253,7 +260,7 @@ class WriteableTable extends Table {
  * This function is used to instantiate a Table object in the context of
  * applying a patch (see apply_patch.js).
  */
-export function instantiateTable(objectId, entries) {
+export function instantiateTable (objectId: ObjectId, entries?: any) {
   const instance = Object.create(Table.prototype)
   instance[OBJECT_ID] = objectId
   instance[CONFLICTS] = Object.freeze({})
